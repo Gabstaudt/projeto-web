5a4e518028f0487a18657d3fe050c6fdb3857409
Gabstaudt <macstaudt@gmail.com>
2024-10-31 09:03:35 -0300

    conversão

--- src/app/models/converter.model.ts
+++ src/app/models/converter.model.ts
@@ -1,31 +1,36 @@
 import { TipoTag } from './tipo.model'; 
 
 export function converterLeitura(tagTipo: TipoTag, valor: number): string {
- 
   console.log('Valores recebidos - tagTipo:', tagTipo, ', valor:', valor);
-  switch (tagTipo) {
-    
 
+  const formatarValor = (valor: number, casasDecimais: number): string => {
+    return valor.toLocaleString('pt-BR', {
+      minimumFractionDigits: casasDecimais,
+      maximumFractionDigits: casasDecimais
+    });
+  };
+
+  switch (tagTipo) {
     case TipoTag.Booleano:
       return valor === 0 ? 'Desligada' : 'Ligada';
     case TipoTag.Vazao0:
-      return `${valor.toFixed(0)} m³/h`; 
+      return `${formatarValor(valor, 0)} m³/h`; 
     case TipoTag.Vazao1:
-      return `${valor.toFixed(2)} m³/h`; 
+      return `${formatarValor(valor / 10, 1)} m³/h`; 
     case TipoTag.Nivel:
-      return `${valor.toFixed(2)} m`; 
+      return `${formatarValor(valor / 100, 2)} m`; 
     case TipoTag.Pressao:
-      return `${valor.toFixed(2)} mca`; 
+      return `${formatarValor(valor/ 100, 2)} mca`; 
     case TipoTag.Volume:
-      return `${valor.toFixed(0)} m³`; 
+      return `${formatarValor(valor, 0)} m³`; 
     case TipoTag.Tensao:
-      return `${valor.toFixed(0)} V`;
+      return `${formatarValor(valor, 0)} V`;
     case TipoTag.Corrente:
-      return `${valor.toFixed(0)} A`; 
+      return `${formatarValor(valor, 0)} A`; 
     case TipoTag.Frequencia:
-      return `${valor.toFixed(0)} Hz`; 
+      return `${formatarValor(valor, 0)} Hz`; 
     case TipoTag.Abertura:
-      return `${valor.toFixed(0)} %`; 
+      return `${formatarValor(valor, 0)} %`; 
     default:
       return 'Valor desconhecido';
   }
